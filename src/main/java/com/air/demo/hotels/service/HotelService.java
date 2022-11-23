d3d6b69952770a50990952d6f5c37568eba89629
mukul0000 <mukulrajput9675@gmail.com>
2022-11-23 09:49:59 +0530

    for hotel service and service imple added

---
+++ src/main/java/com/air/demo/hotels/service/HotelService.java
@@ -0,0 +1,10 @@
+package com.air.demo.hotels.service;
+
+import com.air.demo.utilityDto.responseDto.ResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface HotelService {
+
+     ResponseDto addHotels();
+}
---
+++ src/main/java/com/air/demo/hotels/serviceImpl/HotelServiceImpl.java
@@ -0,0 +1,15 @@
+package com.air.demo.hotels.serviceImpl;
+
+import com.air.demo.hotels.service.HotelService;
+import com.air.demo.utilityDto.responseDto.ResponseDto;
+
+public class HotelServiceImpl implements HotelService {
+
+
+
+    @Override
+    public ResponseDto addHotels() {
+        return null;
+    }
+
+}
